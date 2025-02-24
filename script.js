3c9c1bded1f456c43d8e55adecdb40eb98b80281
Danish Imam <96104496+danishimam@users.noreply.github.com>
2025-02-25 00:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	
 	if (s === "") {
 		return s;
 	}
 	
-	// let result = "";
+	let result = "";
 	
-	// for (let i=0; i<s.length; i++) {
-	// 	if (s[i] === " ") {
-	// 		break;
-	// 	} 
-	// 	result += s[i];
-	// }
-	// return result;
-
-	let words = s.split(" ");
-	return words[0]
+	for (let i=0; i<s.length; i++) {
+		if (s[i] === " ") {
+			break;
+		} 
+		result += s[i];
+	}
+	return result;
 }
 
 // Do not change the code below  
  
-const s = prompt("Enter String:");
-alert(firstWord(s));    
+// const s = prompt("Enter String:");
+// alert(firstWord(s));    
